364f5c7ae2c2ff43efc9de65a09a81c5b0a4b920
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-11-21 19:16:33 +0100

    Update train.py

--- train.py
+++ train.py
@@ -12,6 +12,75 @@ import re
 import argparse
 from tqdm import tqdm
 import os
+import math
+
+
+class Annealer:
+    """
+    This class is used to anneal the KL divergence loss over the course of training VAEs.
+    After each call, the step() function should be called to update the current epoch.
+    """
+
+    def __init__(self, total_steps, shape, baseline=0.0, cyclical=False, disable=False):
+        """
+        Parameters:
+            total_steps (int): Number of epochs to reach full KL divergence weight.
+            shape (str): Shape of the annealing function. Can be 'linear', 'cosine', or 'logistic'.
+            baseline (float): Starting value for the annealing function [0-1]. Default is 0.0.
+            cyclical (bool): Whether to repeat the annealing cycle after total_steps is reached.
+            disable (bool): If true, the __call__ method returns unchanged input (no annealing).
+        """
+        self.total_steps = total_steps
+        self.current_step = 0
+        self.cyclical = cyclical
+        self.shape = shape
+        self.baseline = baseline
+        if disable:
+            self.shape = 'none'
+            self.baseline = 0.0
+
+    def __call__(self, kld):
+        """
+        Args:
+            kld (torch.tensor): KL divergence loss
+        Returns:
+            out (torch.tensor): KL divergence loss multiplied by the slope of the annealing function.
+        """
+        out = kld * self.slope()
+        return out
+
+    def slope(self):
+        if self.shape == 'linear':
+            y = (self.current_step / self.total_steps)
+        elif self.shape == 'cosine':
+            y = (math.cos(math.pi * (self.current_step / self.total_steps - 1)) + 1) / 2
+        elif self.shape == 'logistic':
+            exponent = ((self.total_steps / 2) - self.current_step)
+            y = 1 / (1 + math.exp(exponent))
+        elif self.shape == 'none':
+            y = 1.0
+        else:
+            raise ValueError('Invalid shape for annealing function. Must be linear, cosine, or logistic.')
+        y = self.add_baseline(y)
+        return y
+
+    def step(self):
+        if self.current_step < self.total_steps:
+            self.current_step += 1
+        if self.cyclical and self.current_step >= self.total_steps:
+            self.current_step = 0
+        return
+
+    def add_baseline(self, y):
+        y_out = y * (1 - self.baseline) + self.baseline
+        return y_out
+
+    def cyclical_setter(self, value):
+        if value is not bool:
+            raise ValueError('Cyclical_setter method requires boolean argument (True/False)')
+        else:
+            self.cyclical = value
+        return
 
 class ProfisDataset(Dataset):
     """
@@ -161,7 +230,7 @@ class Profis(nn.Module):
 def vae_loss(x_decoded_mean, y, z_mean, z_logvar):
     xent_loss = F.binary_cross_entropy(x_decoded_mean, y, reduction='mean')
     kl_loss = -0.5 * torch.sum(1 + z_logvar - z_mean.pow(2) - z_logvar.exp())
-    return xent_loss + kl_loss, kl_loss
+    return xent_loss, kl_loss
 
 def is_valid(smiles):
     try:
@@ -173,7 +242,7 @@ def is_valid(smiles):
 def train(model, train_loader, val_loader, epochs=100, device='cuda', lr=0.0001, print_progress=False):
 
     charset = load_charset()
-
+    annealer = Annealer(30, 'cosine', baseline=0.0)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     print('Using device:', device)
 
@@ -190,7 +259,8 @@ def train(model, train_loader, val_loader, epochs=100, device='cuda', lr=0.0001,
             y = y.to(device)
             optimizer.zero_grad()
             output, mean, logvar = model(X)
-            loss, kld_loss = vae_loss(output, y, mean, logvar)
+            recon_loss, kld_loss = vae_loss(output, y, mean, logvar)
+            loss = recon_loss + annealer(kld_loss)
             loss.backward()
             train_loss += loss.item()
             kld_loss += kld_loss.item()
@@ -256,7 +326,7 @@ epochs = args.epochs
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 model = Profis(fp_size=2048 if args.fp_type == 'ECFP' else 4860).to(device)
-model = train(model, train_loader, val_loader, epochs, device, lr=args.lr, print_progress=True)
+model = train(model, train_loader, val_loader, epochs, device, lr=args.lr, print_progress=False)
 
 if os.path.exists('models') is False:
     os.makedirs('models')
