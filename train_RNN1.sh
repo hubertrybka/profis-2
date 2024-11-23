a1ec8c5160ca762cdfd2e347e543ac2f681cd2f6
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-11-23 15:08:12 +0100

    update train

--- train_RNN1.sh
+++ train_RNN1.sh
@@ -3,4 +3,4 @@ source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-python train.py --epochs 500 --batch_size 512 --lr 0.0001 --fp_type ECFP --name profis2_ecfp_lr2e-4
+python train.py --epochs 500 --batch_size 512 --lr 0.0002 --fp_type ECFP --name profis2_ecfp_lr2e-4
