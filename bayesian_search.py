a4213c2878b50c9456ce65a3e35e1ee9c3ab5e6e
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-12-08 10:45:13 +0100

    update cofngi

--- bayesian_search.py
+++ bayesian_search.py
@@ -193,6 +193,7 @@ if __name__ == "__main__":
             f"init_points: {int(config['SEARCH']['n_init'])}",
             f"n_iter: {int(config['SEARCH']['n_iter'])}",
             f"bounds: {bounds}",
+            f"min_window: {float(config['SEARCH']['min_window'])}",
             f"verbosity: {verbosity}",
             f"n_workers: {n_workers}",
             f"time elapsed per sample: {round(time_elapsed / n_samples, 2)} s",
