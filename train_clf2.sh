ec3638bdcab7a8ca4d364284c08afa9d9abca3d4
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-12-09 18:14:50 +0100

    update config add train sscript

--- train_clf2.sh
+++ train_clf2.sh
@@ -7,4 +7,4 @@ source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
 wandb login 505ce3ad45fdf9309c3d8ec1d9764262ae6929c1
-python train_clf.py -c config_files/MLP_config.ini
+python train_clf.py -c config_files/SVC_config.ini
