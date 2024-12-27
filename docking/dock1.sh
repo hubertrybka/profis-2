bea1d8131e3657281083b33dbfc828c4e65dac61
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2024-12-28 00:40:25 +0100

    add scripts

---
+++ docking/dock1.sh
@@ -0,0 +1,6 @@
+cd D2
+python ../dock.py -p ../krfp_profis_bounds2.smi
+python ../dock.py -p ../krfp_profis_bounds1.smi
+python ../dock.py -p ../chembl2.smi
+python ../dock.py -p ../chembl3.smi
+python ../dock.py -p ../chembl4.smi
---
+++ docking/dock2.sh
@@ -0,0 +1,6 @@
+cd esteraze
+python ../dock.py -p ../krfp_profis_bounds2.smi
+python ../dock.py -p ../krfp_profis_bounds1.smi
+python ../dock.py -p ../chembl2.smi
+python ../dock.py -p ../chembl3.smi
+python ../dock.py -p ../chembl4.smi
