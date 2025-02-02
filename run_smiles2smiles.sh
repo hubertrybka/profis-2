fe466c6fb9b22c4a4a2c0e5443ac49e9b7608938
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2025-02-02 03:18:05 +0100

    whatever

--- run_smiles2smiles.sh
+++ run_smiles2smiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=smiles2smiles_anneal
+#SBATCH --job-name=smiles2smiles_anneal_beta_01
 #SBATCH --partition=dgx_A100
 #SBATCH --gpus=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=32G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=16G
 source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
