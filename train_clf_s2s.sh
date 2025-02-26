94b07b8e31d77c2423e20fae93ca942e6c083245
Hubert Rybka <hubert.rybka@doctoral.uj.edu.pl>
2025-02-26 13:27:32 +0100

    Update train_clf_s2s.sh

--- train_clf_s2s.sh
+++ train_clf_s2s.sh
@@ -7,6 +7,3 @@ source /raid/soft/miniconda/bin/activate
 conda init bash
 conda activate profis
 python train_clf_s2s.py -c config_files/MLP_config.ini
-python train_clf_s2s.py -c config_files/RF_config.ini
-python train_clf_s2s.py -c config_files/SVC_config.ini
-python train_clf_s2s.py -c config_files/XGB_config.ini
